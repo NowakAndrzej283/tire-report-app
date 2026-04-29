210cf6743c8cd61bd79927b8f66fb2c0b548a14c
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-04-29 21:09:37 +0200

    modify Add.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import WheelForm from "./components/WheelForm";
 import { type ReportFormData } from "./types";
-import { validateDOT, validateVIN, validateEmail, clampVIN } from "./utils/validation";
+import { type VehicleFieldKey } from "./components/VehicleDetailsForm";
+import {
+  getRawVIN,
+  validateForm,
+} from "./utils/validation";
 import "./App.css";
+import "./components/PopupModal";
+import PopupModal from "./components/PopupModal";
+import LoadingOverlay from "./components/LoadingOverlay";
+import VehicleDetailsForm from "./components/VehicleDetailsForm";
 
 const emptyWheel = {
   tireBrand: "",
   size: "",
-  treadDepth: 0,
+  treadDepth: null,
   dot: "",
   rating: 1,
   notes: "",
@@ -28,16 +36,17 @@ const initialState: ReportFormData = {
 
 function App() {
   const [form, setForm] = useState(initialState);
-  //const [message, setMessage] = useState("");
   const [popupMessage, setPopupMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [shouldResetForm, setShouldResetForm] = useState(false);
 
-  const fields = [
+  const fields: { key: VehicleFieldKey; placeholder: string }[] =
+  [
     { key: "brand", placeholder: "Marka" },
     { key: "model", placeholder: "Model" },
     { key: "vin", placeholder: "VIN" },
-    { key: "email", placeholder: "Email *" },
+    { key: "email", placeholder: "Email" },
   ];
 
   const wheelConfigs = [
@@ -64,103 +73,89 @@ function App() {
     }));
   };
 
-  // Submiting the form
   const submitForm = async () => {
-    // if (!validateVIN(form.vin)) {
-    //   setPopupMessage("Nieprawidłowy numer VIN");
-    //   return;
-    // }
-    // setLoading(true);
-    // setPopupMessage("");
-    // // Simulating the backend request
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
-    // console.log("Zapisany raport:", form);
-    // setPopupMessage("Raport zapisany pomyślnie (test lokalny)");
-    // setForm(initialState);
-    // setLoading(false);
-    setIsSubmitted(true);
+    if (loading) return;
+
+    const rawVIN = getRawVIN(form.vin);
+
+    if (rawVIN.length !== 17) {
+      setPopupMessage(
+        `VIN musi zawierać dokładnie 17 znaków (${rawVIN.length}/17)`
+      );
+      return;
+    }
+    const { errors, warnings } = validateForm(form);
+
+    if (errors.length > 0) {
+      setPopupMessage(errors.join("\n"));
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      let message = "Raport zapisany pomyślnie";
+
+      if (warnings.length > 0) {
+        message += "\n\n Ostrzeżenia dotyczące stanu opon:\n";
+        message += warnings.join("\n");
+      }
+
+      setPopupMessage(message);
+      console.log(form);
+    } catch {
+      setPopupMessage("Wystąpił błąd podczas zapisu formularza");
+    } finally {
+      setLoading(false);
+      setShouldResetForm(true);
+      setIsSubmitted(false);
+      
+    }
   };
 
+  console.log('is submited', isSubmitted);
   return (
-    <form className="app-container">
-      <h1>Raport opon</h1>
+    <>
+      <form className="app-container" onSubmit={(e) => e.preventDefault()}>
+        <h1>Raport stanu opon</h1>
+        <VehicleDetailsForm fields={fields} form={form} setForm={setForm} />
 
-      <div className="form-section">
-        {fields.map(({ key, placeholder }) => {
-          const isVIN = key === "vin";
-          const value = form[key as keyof typeof form] as string;
-
-          return (
-            <div key={key}>
-              <input
-                type={key === "email" ? "email" : "text"}
-                placeholder={placeholder}
-                value={value}
-                onChange={(e) =>
-                  setForm({
-                    ...form,
-                    [key]: isVIN
-                      ? clampVIN(e.target.value.toUpperCase())
-                      : e.target.value,
-                  })
-                }
-                style={{
-                  border:
-                    isVIN && value
-                      ? validateVIN(value)
-                        ? "2px solid #d1d5db"
-                        : "2px solid #ef4444"
-                      : undefined,
-                }}
-                required={key !== "email"}
+        <div className="wheels-grid">
+          {wheelConfigs.map(({ key, label }) => (
+            <div className="wheel-card" key={key}>
+              <WheelForm
+                submitted={isSubmitted}
+                label={label}
+                data={form.wheels[key]}
+                onChange={(f, v) => updateWheel(key, f, v)}
               />
-
-              {isVIN && value && !validateVIN(value) && (
-                <p
-                  style={{
-                    color: "#ef4444",
-                    fontSize: "12px",
-                    marginBottom: "8px",
-                  }}
-                >
-                  VIN musi zawierać dokładnie 17 znaków (bez I, O, Q)
-                </p>
-              )}
             </div>
-          );
-        })}
-        <em style={{ fontSize: "12px" }}>* - pole opcjonalne</em>
-      </div>
-
-      <div className="wheels-grid">
-        {wheelConfigs.map(({ key, label }) => (
-          <div className="wheel-card" key={key}>
-            <WheelForm
-              submitted={isSubmitted}
-              label={label}
-              data={form.wheels[key]}
-              onChange={(f, v) => updateWheel(key, f, v)}
-            />
-          </div>
-        ))}
-      </div>
-
-      <button onClick={submitForm} disabled={loading}>
-        {loading ? "Zapisywanie..." : "Zapisz raport"}
-      </button>
-
-      {popupMessage && (
-        <div className="popup-overlay" onClick={() => setPopupMessage("")}>
-          <div className="popup-modal" onClick={(e) => e.stopPropagation()}>
-            <h3>Informacja</h3>
-            <p>{popupMessage}</p>
-            <button type="submit" onClick={() => setPopupMessage("")}>
-              Zamknij
-            </button>
-          </div>
+          ))}
         </div>
-      )}
-    </form>
+
+        <button
+          onClick={submitForm}
+          disabled={loading || getRawVIN(form.vin).length !== 17}
+        >
+          {loading ? "Zapisywanie..." : "Zapisz raport"}
+        </button>
+      </form>
+
+      {loading && <LoadingOverlay />}
+      <PopupModal
+        message={popupMessage}
+        onClose={() => {
+          setPopupMessage("");
+
+          if (shouldResetForm) {
+            setForm(initialState);
+            window.location.reload();
+            setShouldResetForm(false);
+          }
+        }}
+      />
+    </>
   );
 }
 
